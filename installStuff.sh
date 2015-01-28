bba69c2d7f2b28b60a29f6d4888886b017aa614f
Matt Enterline <matthew.j.enterline@gmail.com>
2015-01-27 20:52:27 -0500

    Rewrote install script, made dirs for each config

---
+++ installStuff.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Argument = -v vim -t tmux -i i3
+
+usage()
+{
+cat << EOF
+usage: $0 [options]
+
+This script moves the configuration files to the specified directories
+
+OPTIONS:
+-h		show this message
+-v		vim config
+-t		tmux config
+-i		i3 window manager config
+EOF
+}
+
+VIMDEST= 
+TMUXDEST= 
+I3DEST= 
+
+while getopts "hv:t:i" OPTION
+do
+	case $OPTION in
+		h)
+			usage
+			exit 1
+			;;
+		v)
+			VIMDEST=$OPTARG
+			cp vim/.vimrc $VIMDEST/
+			git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+			;;
+		t)
+			TMUXDEST=$OPTARG
+			cp tmux/.tmux.conf $TMUXDEST/
+			;;
+		i)
+			I3DEST=$OPTARG
+			cp i3/config $I3DEST/
+			;;
+		?)
+			usage
+			exit 1
+			;;
+	esac
+done
+
